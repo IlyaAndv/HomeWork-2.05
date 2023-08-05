0ecc6b67bd27757696e8aac65f8f0361c8c23b07
Ilya <ktoto47913@gmail.com>
2023-08-05 16:40:28 +0300

    Creating alerts logic

--- HomeWork 2.05/LoginViewController.swift
+++ HomeWork 2.05/LoginViewController.swift
@@ -1,9 +1,33 @@
 import UIKit
 
-class LoginViewController: UIViewController {
+final class LoginViewController: UIViewController {
 
+    @IBOutlet var logInButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        logInButton.layer.cornerRadius = 13
+    }
+    
+    @IBAction func pressButtonForgotUserName() {
+        showAlert(whithTitle: "Warning!!", andMessage: "Your user name is root.")
+    }
+    
+    @IBAction func pressButtonForgotPassword() {
+        showAlert(whithTitle: "Warning!!", andMessage: "Your password is 123.")
+    }
+    
+    private func showAlert(whithTitle title: String, andMessage message: String) {
+        let alert = UIAlertController(
+            title: title,
+            message: message,
+            preferredStyle: .alert
+        )
+        
+        let okButton = UIAlertAction(title: "OK", style: .default)
+        alert.addAction(okButton)
+        
+        present(alert, animated: true)
     }
 
 }
