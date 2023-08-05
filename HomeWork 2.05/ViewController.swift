4d63ac4fd8fdae5cbca623e263af721016ab8e9d
Ilya <ktoto47913@gmail.com>
2023-08-05 12:53:26 +0300

    Initial Settings

--- HomeWork 2.05/ViewController.swift
+++ HomeWork 2.05/ViewController.swift
@@ -1,19 +1,9 @@
-//
-//  ViewController.swift
-//  HomeWork 2.05
-//
-//  Created by Илья on 05.08.2023.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
 
-
 }
-
