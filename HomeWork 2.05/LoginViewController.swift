4095f23d3cbccb7b2f71e38cb545aff7af09090b
Ilya <ktoto47913@gmail.com>
2023-08-05 18:30:43 +0300

    Initial application logic settings

--- HomeWork 2.05/LoginViewController.swift
+++ HomeWork 2.05/LoginViewController.swift
@@ -1,22 +1,37 @@
 import UIKit
 
-final class LoginViewController: UIViewController {
+final class LoginViewController: UIViewController, UITextFieldDelegate {
 
     @IBOutlet var logInButton: UIButton!
+    
+    @IBOutlet var userName: UITextField!
+    @IBOutlet var password: UITextField!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         logInButton.layer.cornerRadius = 13
+        
+        userName.delegate = self
+        password.delegate = self
     }
     
     @IBAction func pressButtonForgotUserName() {
-        showAlert(whithTitle: "Warning!!", andMessage: "Your user name is root.")
+        showAlert(whithTitle: "Warning!", andMessage: "Your user name is root.")
     }
     
     @IBAction func pressButtonForgotPassword() {
-        showAlert(whithTitle: "Warning!!", andMessage: "Your password is 123.")
+        showAlert(whithTitle: "Warning!", andMessage: "Your password is 123.")
     }
     
+    internal func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        if textField == userName {
+            if userName.text?.isEmpty == false {
+                password.becomeFirstResponder()
+            }
+        }
+        return true
+    }
+
     private func showAlert(whithTitle title: String, andMessage message: String) {
         let alert = UIAlertController(
             title: title,
