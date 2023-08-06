aa5b7fe156d8f2b261bda56a74acf1858648b750
Ilya <ktoto47913@gmail.com>
2023-08-06 17:50:06 +0300

    123

--- HomeWork 2.05/LoginViewController.swift
+++ HomeWork 2.05/LoginViewController.swift
@@ -1,29 +1,36 @@
 import UIKit
 
-final class LoginViewController: UIViewController, UITextFieldDelegate {
+final class LoginViewController: UIViewController {
 
+// MARK: - IB Outlets
     @IBOutlet var logInButton: UIButton!
     
     @IBOutlet var loginTF: UITextField!
     @IBOutlet var passwordTF: UITextField!
-    
+
+// MARK: - View Life Cycle
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         logInButton.layer.cornerRadius = 13
         
         loginTF.delegate = self
         passwordTF.delegate = self
-        
-        
     }
     
-    override func shouldPerformSegue(withIdentifier identifier: String, sender: Any?) -> Bool {
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        super .touchesBegan(touches, with: event)
+    }
+    
+    override func shouldPerformSegue(
+        withIdentifier identifier: String,
+        sender: Any?
+    ) -> Bool {
         guard loginTF.text == "root", passwordTF.text == "123" else {
-            showAlert(
+            showAlertForInvalidPassOrLog(
                 whithTitle: "Invalid login or password!",
                 andMessage: "Please, enter correct login or password"
             )
-            
             return false
         }
         return true
@@ -31,44 +38,71 @@ final class LoginViewController: UIViewController, UITextFieldDelegate {
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         let settingsVC = segue.destination as? WelcomeViewController
+        
         settingsVC?.greetings = "Wellcom, \(loginTF.text ?? "")"
     }
-    
+
+// MARK: - IB Actions
     @IBAction func unwind(for segue: UIStoryboardSegue) {
         loginTF.text = ""
         passwordTF.text = ""
     }
     
-    @IBAction func pressButtonForgotUserName() {
-        showAlert(whithTitle: "Warning!", andMessage: "Your user name is root.")
+    @IBAction func pressButtonForgotLogin() {
+        showAlertBut(whithTitle: "Warning!",andMessage: "Your login is root.")
     }
     
     @IBAction func pressButtonForgotPassword() {
-        showAlert(whithTitle: "Warning!", andMessage: "Your password is 123.")
+        showAlertBut(whithTitle: "Warning!",andMessage: "Your password is 123.")
     }
-    
-    internal func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+}
+
+// MARK: - UITextFieldDelegate
+extension LoginViewController: UITextFieldDelegate {
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
         switch textField {
         case loginTF: passwordTF.becomeFirstResponder()
-        default: print("true")
+        default:
+            loginTF.becomeFirstResponder()
+            logInButton.sendActions(for: .touchUpInside)
         }
         return true
     }
+}
 
-    private func showAlert(whithTitle title: String, andMessage message: String) {
+// MARK: - Alert methods
+extension LoginViewController {
+    private func showAlertBut(
+        whithTitle title: String,
+        andMessage message: String
+    ) {
         let alert = UIAlertController(
             title: title,
             message: message,
             preferredStyle: .alert
         )
         
-        let okButton = UIAlertAction(title: "OK", style: .default) { _ in
-            self.loginTF.text = ""
-            self.passwordTF.text = ""
-        }
-        alert.addAction(okButton)
+        alert.addAction(UIAlertAction(title: "OK", style: .default))
         
         present(alert, animated: true)
     }
-
+    
+    private func showAlertForInvalidPassOrLog(
+        whithTitle title: String,
+        andMessage message: String
+    ) {
+        let alert = UIAlertController(
+            title: title,
+            message: message,
+            preferredStyle: .alert
+        )
+        
+        alert.addAction(
+            UIAlertAction(title: "OK", style: .default) {
+                _ in self.passwordTF.text = ""
+            }
+        )
+        
+        present(alert, animated: true)
+    }
 }
